92b2eb8aa3d8ca7ddb618a3950798257bd189c28
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-24 02:12:06 +0600

    class7done

---
+++ class7.js
@@ -0,0 +1,29 @@
+// javascript keyword, variable name naming convention
+
+// 1. no keyword in variable name
+var false = 66;
+var var = 55;
+console.log(var)
+
+// 2. no space or gap in variable name
+var is happy = 44;
+// this can be like this
+var ishappy = 44;
+
+// 3. no quote
+// var 'address' = 'jamalpu';
+
+// 4. can not start with a number
+
+// 5. variable is case sensitive
+
+
+// variable name convention
+
+var myCurrentLocation = 'bangladesh usa uk'
+// camel case
+var my_current_location = 'bangladesh usa uk'; 
+// snake case
+var MyCurrentLocation = 'bangladesh usa uk'; 
+// pascal case
+
