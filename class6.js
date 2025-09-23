53785cfa0573f4aab78d0fc72993d0f4a52ed57e
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-24 01:59:25 +0600

    class6added

---
+++ class6.js
@@ -0,0 +1,18 @@
+// variable data type, numeric, string, boolean
+var name = 'rohan';
+console.log(name)
+var name2 = 'bangladesh from dhaka';
+console.log(name2)
+// string end
+// boolean start
+var isRich = true;
+console.log(isRich)
+var isPoor = false;
+console.log(isPoor)
+console.log(typeof name2)
+console.log(typeof isPoor)
+// boolean end
+// numeric start
+var price = 55;
+console.log(price)
+console.log(typeof price)
