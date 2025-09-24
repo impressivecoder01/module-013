0dd418026ab278c2f11c306d98e6622b59106fd1
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-25 01:06:53 +0600

    class8

---
+++ class8.js
@@ -0,0 +1,15 @@
+// know the fundamental of js numbers
+var mangoPrice = 66;
+var bananaPrice = .55;
+// var applePrice = parseInt('33.9');
+var applePrice = parseFloat('33.9');
+// console.log(bananaPrice)
+console.log(typeof applePrice)
+console.log(applePrice)
+console.log(mangoPrice + applePrice)
+
+var first = 0.1;
+var second = 0.2;
+// console.log(first + second )
+var total = first + second;
+console.log(parseFloat(total.toFixed(1)))
